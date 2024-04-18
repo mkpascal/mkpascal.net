7a2082f26715c8048bce7220e63c14184c814e29
Pascal M <pascal@khot.us>
2024-04-18 13:13:25 -0700

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import styles from '../styles/resume.module.css';
