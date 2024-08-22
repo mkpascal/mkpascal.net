2305c332cd3ef3638a125c2c64b65bb912dda63d
Pascal <pascal@khot.us>
2024-08-22 10:47:34 -0700

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -14,7 +14,7 @@ export default function Resume() {
                 <div className={utilStyles.list}>
                     <div>
                         <p className={styles.topLink}>
-                            <Link href={`https://www.mkpascal.net/se_resume.pdf`}>
+                            <Link href={`https://www.mkpascal.net/Pascal-Muang-Khot-Resume.pdf`}>
                                 <i className="fas fa-file-download"></i> Download PDF Resume
                             </Link>
                         </p>
@@ -32,19 +32,18 @@ export default function Resume() {
                                 <h4>Fandom - Software Engineer - Back-End Developer</h4>
                                 <p>2022 - 2023</p>
                                 <ul>
-                                    <li>Managed a high-volume custom CMS for Metacritic & TV Guide sites, serving millions of users.</li>
-                                    <li>Engineered a robust transcoding framework, alongside content and commerce features.</li>
-                                    <li>Played a key role in refining front-end usability through API development contributions.</li>
+                                    <li>Managed a high-volume custom CMS for Metacritic & TV Guide sites, serving 40 millions of monthly visitors.</li>
+                                    <li>Engineered a robust transcoding framework using JWP, alongside content and commerce features.</li>
+                                    <li>Played a key role in refining front-end usability through API optimization, which contributed to improved SEO scores by enhancing page speed rankings.</li>
                                 </ul>
                             </div>
                             <div>
                                 <h4>Red Ventures - Software Engineer - Full-Stack Developer</h4>
                                 <p>2020 - 2022</p>
                                 <ul>
-                                    <li>Maintained a high-traffic CMS in-house platform, supporting millions of users for CNET & ZDNET.</li>
+                                    <li>Maintained a high-traffic CMS in-house platform, supporting 75 millions of monthly users for CNET & ZDNET.</li>
                                     <li>Undertook responsibilities involving front door, articles, and ads features for both brands.</li>
                                     <li>Revamped the CMS UI/UX offering innovative functionalities and robust commerce integration.</li>
-                                    <li>Demonstrated exceptional proficiency in debugging, data operations, and system integration.</li>
                                     <li>Architected seamless API solutions to bridge the gap between front-end and back-end systems.</li>
                                 </ul>
                             </div>
@@ -52,25 +51,18 @@ export default function Resume() {
                                 <h4>Paramount - Software Engineer - Back-End Developer</h4>
                                 <p>2016 - 2020</p>
                                 <ul>
-                                    <li>Led development of the full back-end platform with Rails as a site engineer for Chowhound.</li>
-                                    <li>Orchestrated a large-scale migration to GCP, ensuring seamless scalability and reliability.</li>
-                                    <li>Engineered a robust user account system and streamlined mail and queue management.</li>
-                                    <li>Expanding reach and accessibility through Wordpress and Apple News partnerships.</li>
-                                    <li>Pioneered the implementation of a place system through strategic third-party collaborations.</li>
-                                    <li>Revolutionized our legacy stack by dockerizing it and fine-tuning for maximum scalability.</li>
-                                    <li>Elevated server performance by meticulously fine-tuning Apache and Redis for maximum efficiency.</li>
-                                    <li>Implemented a sophisticated branching strategy, fostering a streamlined development process.</li>
+                                    <li>Led development of the full back-end platform with Rails as a site engineer for Chowhound, reaching 3 million monthly users.</li>
+                                    <li>Orchestrated a large-scale migration to GCP, updating legacy stack to Docker, ensuring seamless scalability and reliability. Elevated server performance by meticulously fine-tuning Apache and Redis for maximum efficiency.</li>
+                                    <li>Engineered a robust user account system, managed Wordpress and Apple partnerships and streamlined mail and queue management.</li>
                                 </ul>
                             </div>
                             <div>
                                 <h4>Trulify - Software Engineer - Full-Stack Developer</h4>
                                 <p>2013 - 2015</p>
                                 <ul>
-                                    <li>Developed and built Trulify and FairLoan apps using Rails, delivering scalable and robust solutions.</li>
+                                    <li>Developed and built internal apps using Rails, delivering scalable and robust solutions.</li>
                                     <li>Engineered API connections with payroll service bureaus, enabling automated data exchange.</li>
-                                    <li>Implemented a payment infrastructure, efficient subscription management, and billing.</li>
-                                    <li>Enhanced user engagement by integrating Twilio's SMS system, ensuring seamless communication.</li>
-                                    <li>Migrated the frontend framework to Bootstrap, improving the user experience and responsiveness.</li>
+                                    <li>Implemented a payment infrastructure, efficient subscription management, SMS system and billing.</li>
                                     <li>Designed and implemented secure data exchange protocols, safeguarding sensitive PII.</li>
                                 </ul>
                             </div>
@@ -85,45 +77,11 @@ export default function Resume() {
                         <div className={styles.rightColumn}>
                             <h3>SKILLS</h3>
                             <div>
-                                <h4>Languages</h4>
-                                <p>Ruby | Javascript | PHP | HTML | CSS | Sass | Dart | Shell scripting</p>
+                                <p>With a decade of experience in web development serving 100+ millions of users for Fortune 500 and startup companies, I specialize in backend operations and possess frontend development and devops expertise. 
+<br/><br/>Skilled in <b>Ruby on Rails, Javascript, Node.js, Next.js, Hotwire Turbo, React, Electron, CSS/SASS/SCSS, Bootstrap, HTML, PHP, Docker, Cloudflare, AWS, GitHub Actions, PostgreSQL, MySQL, Redis, RSpec</b>.
+    </p>
                             </div>
-                            <div>
-                                <h4>Frameworks</h4>
-                                <p>Rails | Sinatra | Node.js | Bootstrap | Next.js | React | Electron | Flutter</p>
-                            </div>
-                            <div>
-                                <h4>Libraries</h4>
-                                <p>Sidekiq | Devise | Sendgrid | Postmark | ActiveAdmin | llama.cpp | Dart</p>
-                            </div>
-                            <div>
-                                <h4>DevOps</h4>
-                                <p>Docker | GCP | Cloudflare | Fastly | Heroku | AWS | OVH | Digitalocean | New Relic</p>
-                            </div>
-                            <div>
-                                <h4>Integrations</h4>
-                                <p>GitHub Actions | Jenkins | RSpec | Capybara | Rubocop | Selenium</p>
-                            </div>
-                            <div>
-                                <h4>Databases</h4>
-                                <p>PostgreSQL | Redis | MongoDB | MySQL | SQLite | Solr | Memcached</p>
-                            </div>
-                            <div>
-                                <h4>System Administration</h4>
-                                <p>Debian, Ubuntu, OS X Server, Windows Server, Bash, Powershell</p>
-                            </div>
-                            <div>
-                                <h4>Databases</h4>
-                                <p>Postgres, Redis, MongoDB, MySQL, SQLite, Solr</p>
-                            </div>
-                            <div>
-                                <h4>Methodologies</h4>
-                                <p>Pair programming | Code reviews | Facilitating processes | Directing engineers</p>
-                            </div>
-                            <div>
-                                <h4>Miscellaneous</h4>
-                                <p>Agile | Scrum | Gitflow | Extreme programming | Test-driven development | Gitflow</p>
-                            </div> 
+
                             <div>
                                 <h3>EDUCATION</h3>
                                 <div>
